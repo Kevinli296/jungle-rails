ee5fb184624902afe1ab22251262cf3d7e658fb5
Kevinli296 <kevinli296@outlook.com>
2021-01-13 03:50:00 +0000

    Add validations and change sessions controller to implement new authenticate_with_credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
   validates :password_digest, presence: true
 
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+
 end
