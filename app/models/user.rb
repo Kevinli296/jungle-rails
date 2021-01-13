26ae3532fadfe0036586b95bbba5fec635b68228
Kevinli296 <kevinli296@outlook.com>
2021-01-13 00:53:00 +0000

    Add validation specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
+  validates :password, length: { minimum: 6 }
+  validates :password_digest, presence: true
+
 end
