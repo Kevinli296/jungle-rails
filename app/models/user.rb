c7836af1f5f69e19a077165f87bb9cf066457254
Kevinli296 <kevinli296@outlook.com>
2021-01-13 00:02:28 +0000

    Configure validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  valdiates: :email, presence: true, uniqueness: { case_sensitive: false }
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new({:email => 'testing@test.com'})
+  end
+  # describe 'Validations' do
+  #   it 'should have matching password and password_confirmation' do
+
+  #   end
+  # end
 end
